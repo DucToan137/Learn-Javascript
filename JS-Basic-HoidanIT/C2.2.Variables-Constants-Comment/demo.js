cf178fb8055bcff115f294ab9870ae127837afdd
Duc Toan <22120376@student.hcmus.edu.vn>
2024-07-31 21:16:49 +0700

    Data type

---
+++ JS-Basic-HoidanIT/C2.2.Variables-Constants-Comment/demo.js
@@ -0,0 +1,22 @@
+
+
+let a = `Hello world from javascript`;
+console.log(a);
+
+// let x;
+// x = 10; // x is a number
+// x = "hello"; // x is a string
+
+// String
+// let carName = "Volvo";
+// let carName2 = 'Volvo';
+// let carName3 = `Volvo`;
+
+
+// const y = 5;
+// y = 6;
+// console.log(y); // Error
+
+let x = 5, y = 6, z = 7;
+console.log("Test: ","x=", x," y=", y," z=", z);
+console.log("Test: " + "x=" + x + " y=" + y + " z=" + z);
