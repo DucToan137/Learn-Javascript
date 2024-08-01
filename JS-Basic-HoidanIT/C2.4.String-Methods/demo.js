d7f67316b4c551bc7228c38255c867763150ddf6
Duc Toan <22120376@student.hcmus.edu.vn>
2024-08-02 00:16:18 +0700

    String methods

---
+++ JS-Basic-HoidanIT/C2.4.String-Methods/demo.js
@@ -0,0 +1,14 @@
+
+let a = `Hello world from HTML`;
+
+console.log(a);
+console.log(`Length a:`, a.length);
+
+let b = a.toUpperCase();
+console.log(b);
+
+let c = a.toLowerCase();
+console.log(c);
+
+let d = a.slice(6, 11);
+console.log(d);
