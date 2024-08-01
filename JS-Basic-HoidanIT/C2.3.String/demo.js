92ccfcf09573eeb317dfc5e86bb86d3bad9d2a65
Duc Toan <22120376@student.hcmus.edu.vn>
2024-08-01 17:58:49 +0700

    String

---
+++ JS-Basic-HoidanIT/C2.3.String/demo.js
@@ -0,0 +1,9 @@
+
+let a = 'String - chuỗi'
+
+console.log(a);
+console.log(4 + 5 + 'a');
+console.log('a' + 4 + 5);
+console.log(a.length); // độ dài chuỗi a
+let text = `We are the so-called "Vikings" from the north.`;
+console.log(text);
