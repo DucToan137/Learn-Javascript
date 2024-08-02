7623c4e91e7b2c84b0076f96ee1a80e89dcb96da
Duc Toan <22120376@student.hcmus.edu.vn>
2024-08-02 21:31:48 +0700

    String template

---
+++ JS-Basic-HoidanIT/C2.6.String-Template/demo.js
@@ -0,0 +1,17 @@
+console.log(`Hello world from HTML`);
+
+
+const a = 5;
+const b = 10;
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+console.log("Fifteen is " + a + b + " and\nnot " + (2 * a + b) + ".");
+
+
+const c = 5;
+const d = 10;
+console.log(`Fifteen is ${c + d} and
+not ${2 * c + d}.`);
+
+// url: '/demo/5'
+console.log(`Check string: a = ${a} b = ${b} a+b = ${a+b}`);
+console.log(`/demo/${a}`);
