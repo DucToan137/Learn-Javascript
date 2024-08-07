920e6dfbe3520f8d81196c6d8c85022f58295b18
Duc Toan <22120376@student.hcmus.edu.vn>
2024-08-07 21:24:31 +0700

    Array

---
+++ JS-Basic-HoidanIT/C2.8.Array/demo.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+
+
+let a ={
+    name: 'Eric',
+    age: 20
+}; // key: value
+
+console.log('check object', a);
+
+let b = ['Bayern', 'Dortmund', 'MU'];
+console.log('typeof ', typeof b, b);
+console.log('second item: ', b[1]);
+
+let c = [
+    {name: 'Eric', age: 20},
+    {name: 'Huy', age: 21}
+];
+console.log('c: ', c);
+
