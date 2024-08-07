88fed8a92b2dd69be5d787a6578d852a02b54fa9
Duc Toan <22120376@student.hcmus.edu.vn>
2024-08-07 23:01:43 +0700

    For Loops

---
+++ JS-Basic-HoidanIT/C3.0.For-Loops/demo.js
@@ -0,0 +1,8 @@
+console.log('Hello world!');
+
+
+let arr = ['MU', 'Arsenal', 'MC', 'Chelsea', 'Liverpool'];
+
+for(let i = 0; i < arr.length; i++) {
+    console.log('check variable i = ', arr[i]);
+}
