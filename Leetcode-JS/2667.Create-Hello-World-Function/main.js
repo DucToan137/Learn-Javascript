aa8d5a6d5a2453eb1bca91520dac99c93b1b6459
Duc Toan <22120376@student.hcmus.edu.vn>
2024-08-09 09:42:58 +0700

    Time: 53 ms (40.86%) | Memory: 48.53 MB (70.13%)

---
+++ Leetcode-JS/2667.Create-Hello-World-Function/main.js
@@ -0,0 +1,12 @@
+
+var createHelloWorld = function () {
+    console.log('Hello World')
+    return function (...args) {
+        return 'Hello World';
+    }
+};
+
+
+const f = createHelloWorld();
+f(); // Hello World
+console.log(f()); // Hello World
